a66750b18c32d240464593043f2adfa103aacf1f
NicolasJamar <nicolas.jamar@gmail.com>
2017-11-27 12:28:41 +0100

    footer 100px

---
+++ css/script.js
@@ -0,0 +1,53 @@
+function show1() {
+    var x = document.getElementById("content1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function show2() {
+    var x = document.getElementById("content2");
+   	if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function show3() {
+    var x = document.getElementById("content3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function show4() {
+    var x = document.getElementById("content4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function show5() {
+    var x = document.getElementById("content5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function show6() {
+    var x = document.getElementById("content6");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
